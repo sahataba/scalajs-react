895604c0e1590a061c82c9e83afe80e957100800
Rudolf Markulin <rudolf.markulin@gmail.com>
2014-12-22 00:06:10 +0100

    binded react to autowire

--- client/src/main/scala/example/ReactExample.scala
+++ client/src/main/scala/example/ReactExample.scala
@@ -1,11 +1,33 @@
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom.{HTMLInputElement, console, document, window, Node}
+
+import scala.concurrent.{Future,Await}
+import scala.concurrent.duration._
+import scalajs.concurrent.JSExecutionContext.Implicits.runNow
+
 import japgolly.scalajs.react._
 import vdom.ReactVDom._
 import all._
 
 import example._
+
+import upickle._
+import autowire._
+
+@JSExport
+object Client extends autowire.Client[String, upickle.Reader, upickle.Writer]{
+  override def doCall(req: Request): Future[String] = {
+    console.log("miha")
+    org.scalajs.dom.extensions.Ajax.post(
+      url = "/api/" + req.path.mkString("/"),
+      data = upickle.write(req.args)
+    ).map(_.responseText)
+  }
+
+  def read[Result: upickle.Reader](p: String) = upickle.read[Result](p)
+  def write[Result: upickle.Writer](r: Result) = upickle.write(r)
+}
 
 @JSExport
 object ReactExamples {
@@ -43,20 +65,25 @@ object ReactExamples {
     }
   }
 
-  //val initial = example.Client[example.Api].list("").toList
-
   val TodoApp = ReactComponentB[Unit]("TodoApp")
-    .initialState(State(Nil, ""))
+    .getInitialState(p => {
+      State(List(User("Iva"), User("Maja")), "")
+    })
     .backend(new Backend(_))
     .render((_,S,B) =>
-    div(
-      h3("TODO"),
-      TodoList(S.users),
-      form(onsubmit ==> B.handleSubmit)(
-        input(onchange ==> B.onChange, value := S.text),
-        button("Add #", S.users.length + 1)
+      div(
+        h3("TODO"),
+        TodoList(S.users),
+        form(onsubmit ==> B.handleSubmit)(
+          input(onchange ==> B.onChange, value := S.text),
+          button("Add #", S.users.length + 1)
+        )
       )
-    )
-  ).buildU
+    ).componentDidMount(scope => {
+      val fetched = Client[example.Api].list("").call().map(_.toList)
+      fetched.map( users =>
+        scope.modState(_ => State(users, ""))
+      )
+    }).buildU
 
 }
