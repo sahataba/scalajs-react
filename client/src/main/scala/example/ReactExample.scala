bfc20b65043b27c3baee853adc92c8fa52d46c35
Rudolf Markulin <rudolf.markulin@gmail.com>
2014-12-21 21:20:57 +0100

    todo react component

--- client/src/main/scala/example/ReactExample.scala
+++ client/src/main/scala/example/ReactExample.scala
@@ -18,6 +18,40 @@ object ReactExamples extends {
     val HelloMessage = ReactComponentB[String]("HelloMessage")
     .render(name => div("Hello ", name))
     .build
-    React.renderComponent(HelloMessage("John"), mountNode)
+    React.renderComponent(TodoApp(), mountNode)
   }
+
+
+  //todo example
+  val TodoList = ReactComponentB[List[String]]("TodoList")
+  .render(P => {
+    def createItem(itemText: String) = li(itemText)
+    ul(P map createItem)
+  })
+  .build
+
+  case class State(items: List[String], text: String)
+
+  class Backend(t: BackendScope[Unit, State]) {
+    def onChange(e: ReactEventI) =
+      t.modState(_.copy(text = e.target.value))
+    def handleSubmit(e: ReactEventI) = {
+      e.preventDefault()
+      t.modState(s => State(s.items :+ s.text, ""))
+    }
+  }
+  val TodoApp = ReactComponentB[Unit]("TodoApp")
+    .initialState(State(Nil, ""))
+    .backend(new Backend(_))
+    .render((_,S,B) =>
+    div(
+      h3("TODO"),
+      TodoList(S.items),
+      form(onsubmit ==> B.handleSubmit)(
+        input(onchange ==> B.onChange, value := S.text),
+        button("Add #", S.items.length + 1)
+      )
+    )
+  ).buildU
+
 }
