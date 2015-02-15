e1e6cdc179dca8c47728b1361eaa4c452f8767f3
Rudolf Markulin <rudolf.markulin@gmail.com>
2015-02-15 20:53:48 +0100

    crud in progrès

--- server/src/main/scala/example/Model.scala
+++ server/src/main/scala/example/Model.scala
@@ -4,10 +4,15 @@ import scala.slick.driver.H2Driver.api._
 
 import scala.slick.lifted.{ProvenShape, ForeignKeyQuery}
 import scala.concurrent._
+import scala.slick.driver.JdbcProfile
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class Users(tag: Tag) extends Table[User](tag, "USERS") {
+abstract class TableWithId[A](tag:Tag, name:String) extends Table[A](tag:Tag, name) {
+  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+}
+
+class Users(tag: Tag) extends TableWithId[User](tag, "USERS") {
 
   // And a ColumnType that maps it to Int values 1 and 0
   implicit val boolColumnType = MappedColumnType.base[Date, Long](
@@ -27,9 +32,6 @@ class Users(tag: Tag) extends Table[User](tag, "USERS") {
     (email:Email) => email.email, example.Email apply _
   )
 
-  // Auto Increment the id primary key column
-  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-  // The name can't be null
   def firstName = column[String]("FIRST_NAME", O.NotNull)
   def lastName = column[String]("LAST_NAME", O.NotNull)
   def email = column[example.Email]("EMAIL", O.NotNull)
@@ -44,34 +46,43 @@ class Users(tag: Tag) extends Table[User](tag, "USERS") {
 }
 
 // The main application
-object TableModel extends CRUD[User]{
+object TableModel extends CRUD[User, Users]{
+
+  val profile = scala.slick.driver.H2Driver
 
   // The query interface for the Suppliers table
-  val users: TableQuery[Users] = TableQuery[Users]
+  val table: TableQuery[Users] = TableQuery[Users]
 
   lazy val db = Database.forURL("jdbc:h2:mem:hello;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
 
   db.run(Action.seq(
-    users.ddl.create,
-    users += User(firstName = "aurelius", lastName = "livingston", email = Email("great road"), birthday = example.Date(1l), role = Admin, status = Pending)
+    table.ddl.create,
+    table += User(firstName = "aurelius", lastName = "livingston", email = Email("great road"), birthday = example.Date(1l), role = Admin, status = Pending)
   ))
 
-  def list2:Future[Seq[User]] = db.run(users.result)
-  def create(user:User):Future[Int] = db.run((users returning users.map(_.id)) += user)
-  def remove(id:Id) = db.run(users.filter(_.id === id).delete)
-  def update(id:Id, upd:User => User) = {
+  def list2:Future[Seq[User]] = db.run(table.result)
+  def remove(id:Id):Future[Int] = db.run(table.filter(_.id === id).delete)
+
+}
+
+trait CRUD[E,T <: TableWithId[E]] {
+  val profile:JdbcProfile
+  val db:Database
+  import profile.api._
+  val table: TableQuery[T]
+
+  type Id = Option[Int]
+
+  def create(entity:E):Future[Int] = db.run((table returning table.map(_.id)) += entity)
+
+  def remove(id:Id):Future[Int]
+
+  def update(id:Id, upd: E => E):Future[E] =  {
     val act = for {
-      user <- users.filter(_.id === id).result.head
-      updated = upd(user)
-      _ <- users.filter(_.id ===id).update(updated)
+      ent <- table.filter(_.id === id).result.head
+      updated = upd(ent)
+      _ <- table.filter(_.id ===id).update(updated)
     } yield updated
     db.run(act)
   }
-}
-
-trait CRUD[E] {
-  type Id = Option[Int]
-  def create(entity:E):Future[Int]
-  def remove(id:Id):Future[Int]
-  def update(id:Id, upd: E => E):Future[E]
 }
