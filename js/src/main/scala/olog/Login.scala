e777c84b2ca4968c61cf42f18a991b708db5c27c
Rudolf Markulin <rudolf.markulin@gmail.com>
2015-07-06 13:37:51 +0200

    onChangePassword

--- js/src/main/scala/olog/Login.scala
+++ js/src/main/scala/olog/Login.scala
@@ -9,35 +9,50 @@ object LoginPage {
   class Backend($: BackendScope[Unit, State]) {
     def onChangeEmail(e: ReactEventI) =
       $.modState(st => st.copy(credentials = Credentials._email.set(e.target.value)(st.credentials)))
+
+    def onChangePassword(e: ReactEventI) =
+      $.modState(st => st.copy(credentials = Credentials._password.set(e.target.value)(st.credentials)))
+
+    def handleSubmit(e: ReactEventI) = {
+
+    }
   }
 
   val component = ReactComponentB[Unit]("Login")
-    .render(P => {
-    <.div(
-      <.form(
-        ^.cls := "col s12",
-        <.div(
-          ^.cls := "row",
+    .initialState(State(None, Credentials("","")))
+    .backend(new Backend(_))
+    .render((P, S, B) =>
+      <.div(
+        <.form(
+          ^.cls := "col s12",
           <.div(
             ^.cls := "row",
             <.div(
-              ^.cls := "input-field col s12",
-              <.input(^.id := "password", ^.`type` := "password", ^.cls := "validate"),
-              <.label(^.`for` := "password", "Password")
-            )
-          ),
-          <.div(
-            ^.cls := "row",
+              ^.cls := "row",
+              <.div(
+                ^.cls := "input-field col s12",
+                <.input(^.id := "password", ^.`type` := "password", ^.cls := "validate",  ^.onChange ==> B.onChangePassword, ^.value := S.credentials.password),
+                <.label(^.`for` := "password", "Password")
+              )
+            ),
             <.div(
-              ^.cls := "input-field col s12",
-              <.input(^.id := "email", ^.`type` := "email", ^.cls := "validate"),
-              <.label(^.`for` := "email", "Email")
+              ^.cls := "row",
+              <.div(
+                ^.cls := "input-field col s12",
+                <.input(^.id := "email", ^.`type` := "email", ^.cls := "validate", ^.onChange ==> B.onChangeEmail, ^.value := S.credentials.email),
+                <.label(^.`for` := "email", "Email")
+              )
+            ),
+            <.div(
+              ^.cls := "row",
+              <.div(
+                <.a(^.id := "login", ^.`type` := "button", ^.cls := "waves-light btn", ^.onClick ==> B.handleSubmit)("Login")
+              )
             )
           )
         )
       )
-    )
-  }).buildU
+  ).buildU
 
   def apply() = component()
 
