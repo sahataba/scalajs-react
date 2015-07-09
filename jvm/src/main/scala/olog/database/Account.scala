bfc53d09f475729e6c6d3440222447f47150e412
Rudolf Markulin <rudolf.markulin@gmail.com>
2015-07-09 18:20:08 +0200

    moving columns i db layer

--- jvm/src/main/scala/olog/database/Account.scala
+++ jvm/src/main/scala/olog/database/Account.scala
@@ -7,28 +7,7 @@ import scala.concurrent._
 
 class Users(tag: Tag) extends TableWithId[Account.User](tag, "USERS") {
 
-  implicit val idColumnType = MappedColumnType.base[Id[Account.User], Int](
-   b => b.value,
-   id => Id(id)
-  )
-
-  // And a ColumnType that maps it to Int values 1 and 0
-  implicit val boolColumnType = MappedColumnType.base[Date, Long](
-  { b => b.utc},    // map Bool to Int
-  { utc => Date(utc) } // map Int to Bool
-  )
-
-  implicit val boolColumnType2 = MappedColumnType.base[olog.Role, String](
-    RoleConverter.write, (str:String) => RoleConverter.read(str).right.get
-  )
-
-  implicit val statusColumnType2 = MappedColumnType.base[olog.Status[Account.User], String](
-    StatusConverter.write, (str:String) => StatusConverter.read(str).right.get
-  )
-
-  implicit val emailColumnType2 = MappedColumnType.base[olog.Email, String](
-    (email:Email) => email.email, olog.Email apply _
-  )
+  import Columns._
 
   def firstName = column[String]("FIRST_NAME")
   def lastName = column[String]("LAST_NAME")
---
+++ jvm/src/main/scala/olog/database/Columns.scala
@@ -0,0 +1,29 @@
+package olog.database
+
+import slick.driver.H2Driver.api._
+import olog._
+
+object Columns {
+  implicit val idColumnType = MappedColumnType.base[Id[Account.User], Int](
+    b => b.value,
+    id => Id(id)
+  )
+
+  // And a ColumnType that maps it to Int values 1 and 0
+  implicit val boolColumnType = MappedColumnType.base[Date, Long](
+  { b => b.utc},    // map Bool to Int
+  { utc => Date(utc) } // map Int to Bool
+  )
+
+  implicit val boolColumnType2 = MappedColumnType.base[olog.Role, String](
+    RoleConverter.write, (str:String) => RoleConverter.read(str).right.get
+  )
+
+  implicit val statusColumnType2 = MappedColumnType.base[olog.Status[Account.User], String](
+    StatusConverter.write, (str:String) => StatusConverter.read(str).right.get
+  )
+
+  implicit val emailColumnType2 = MappedColumnType.base[olog.Email, String](
+    (email:Email) => email.email, olog.Email apply _
+  )
+}
